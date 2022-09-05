17b4bb39cd5f36e424839743fb463399fd44247d
adnantech17 <adnantech17@gmail.com>
2022-09-05 16:08:45 +0600

    Feat: NVIC functions and datastructures declared

--- src/kern/arch/stm32f446re/sys/sys.c
+++ src/kern/arch/stm32f446re/sys/sys.c
@@ -41,3 +41,74 @@ void SysTick_Handler(void)
     const uint32_t clock = 180000000;
     mscount += 1000/(clock/(SysTick->LOAD+1));
 }
+
+
+void __NVIC_SetPriority(enum IRQn_TypeDef IRQn,uint32_t priority)
+{
+
+}
+uint32_t __NVIC_GetPriority(enum IRQn_TypeDef IRQn)
+{
+
+}
+uint32_t __NVIC_GetActive(enum IRQn_TypeDef IRQn)
+{
+
+}
+void __NVIC_EnableIRQn(enum IRQn_TypeDef IRQn)
+{
+
+}
+void __NVIC_DisableIRQn(enum IRQn_TypeDef IRQn)
+{
+
+}
+
+uint32_t __get_pending_IRQn(enum IRQn_TypeDef IRQn)
+{
+
+}
+void __clear_pending_IRQn(enum IRQn_TypeDef IRQn)
+{
+
+}
+
+void __enable_irq()
+{
+
+}
+void __disable_irq()
+{
+
+}
+
+void __unset_BASEPRI(uint32_t value)
+{
+
+}
+void __set_BASEPRI(uint32_t value)
+{
+
+}
+
+uint32_t __get_PRIMASK()
+{
+
+}
+void __set_PRIMASK(uint32_t priMask)
+{
+
+}
+uint32_t __get_FAULTMASK()
+{
+
+}
+
+void __enable_fault_irq()
+{
+
+}
+void __disable_fault_irq()
+{
+
+}
